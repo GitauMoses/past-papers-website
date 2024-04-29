9cad5b7f1e7fa4d291d8a4ae25e489582bec8a4d
Moses Gitau <121388655+musa-web-pro@users.noreply.github.com>
2024-04-29 13:04:27 +0300

    Refactor app.py to include login and signup routes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import*
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello, Moses'
+def login():
+    return render_template('index.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
